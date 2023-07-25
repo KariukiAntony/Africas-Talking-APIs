20b3d947fc0fa94cfe824badbe30eeed3d33de50
KariukiAntony <antonygichoya9@gmail.com>
2023-07-25 17:44:05 +0300

    added  a report form

--- App/send_bulk_sms.py
+++ App/send_bulk_sms.py
@@ -1,12 +1,13 @@
-# Here we are going to send sms to a list of phone number usings the same criteria
-# The only requirement we need is a list of phone numbers and the message.
+""" Here we are going to send sms to a list of phone number usings the same criteria
+
+    The only requirement we need is a list of phone numbers and the message.
+ """
 
 recepients = ["+254712345678", "+254712345677", "+254712345676", "+254712345675"]
 
 message = "Hello there, this is africas talking sms api"
 
 from send_sms_sandbox import send_sms
-
 
 
 def handle_bulk_sms():
