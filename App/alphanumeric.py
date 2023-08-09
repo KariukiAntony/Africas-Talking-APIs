f105eda80a8b34218c3fb3df5e48c5321df58180
KariukiAntony <antonygichoya9@gmail.com>
2023-08-09 14:07:37 +0300

    added a new file

---
+++ App/alphanumeric.py
@@ -0,0 +1,8 @@
+""" Here we are going to send an sms using an alphanumeric """
+""" while using this alphanumeric/sender id, a customer cannot reply to this code"""
+""" make sure you have a registered alphanumeric """
+
+""" enough of the talk, lets code """
+
+
+ 
