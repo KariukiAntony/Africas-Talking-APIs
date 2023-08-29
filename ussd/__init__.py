8b30d9e5a40b02979715b28fee800096af831a3c
KariukiAntony <antonygichoya9@gmail.com>
2023-08-29 15:36:55 +0300

    handle first channel of ussd

--- ussd/__init__.py
+++ ussd/__init__.py
@@ -1,1 +1,10 @@
 """ make sure to go through the readme file for further explanation """
+
+from flask import Flask
+from ussd.ussd import ussd
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(ussd)
+    return app
---
+++ ussd/ussd.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request
+
+ussd = Blueprint("ussd", __name__)
+
+response = ""
+user_input = ""
+
+@ussd.post("/handle_ussd")
+def handle_ussd_app():
+    global response
+    global user_input
+    session_id = request.values.get("sessionId")
+    service_code = request.values.get("serviceCode")
+    phone_number = request.values.get("phoneNumber")
+    text = request.values.get("text")
+    user_input += text
+
+    if text == "":
+        response += "CON Choose what you want to check\n"
+        response += "1. Relationship Status\n"
+        response += "2. More Info\n"
+
+    return  response
